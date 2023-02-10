cdd4b4826bdccc2eb46bd642075d7a5c37e007dd
grynka <ihor.kozhemyakin@gmail.com>
2023-02-10 12:51:20 +0200

    upd

--- contacts.js
+++ contacts.js
@@ -1,34 +1,69 @@
 const fs = require("fs/promises");
-const path = require("path")
-const contactPatch = path.resolve('./db/contacts.json');
+const path = require("path");
+const contactPatch = path.resolve("./db/contacts.json");
 
 function listContacts() {
-    console.log(contactPatch)
-    fs.readFile(contactPatch)
-  .then(data => console.log(data.toString()))
-  .catch(err => console.log(err.message));
-  }
-  
-  function getContactById(contactId) {
-    fs.readFile(contactPatch)
-        .then(data => {
-        contacts = data.toString()
-        console.log(contacts)
-    })
-    .catch(err => console.log(err.message));
-  }
-  
-  function removeContact(contactId) {
-    // ...твій код
-  }
-  
-  function addContact(name, email, phone) {
-    // ...твій код
-  }
+  console.log(contactPatch);
+  fs.readFile(contactPatch)
+    .then((data) => console.log(data.toString()))
+    .catch((err) => console.log(err.message));
+}
 
-  module.exports = {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact
+function getContactById(contactId) {
+  console.log("get id " + contactId);
+  fs.readFile(contactPatch)
+    .then((data) => JSON.parse(data))
+    .then((contacts) => {
+      console.log(contacts.filter((item) => item.id.includes(contactId)));
+    })
+    .catch((err) => console.log(err.message));
+}
+
+function removeContact(contactId) {
+  console.log("del id " + contactId);
+  fs.readFile(contactPatch)
+    .then((data) => JSON.parse(data))
+    .then((contacts) => {
+      contacts.push(contacts.filter((contact) => contact.id != contactId))
+      fs.writeFile(contactPatch, JSON.stringify(contacts))
+      .then(() => {
+        console.log("Append Success");
+      })
+      .catch((err) => {
+        console.log("Append Failed: " + err);
+      });      
+    })
+    .catch((err) => console.log(err.message));
+}
+
+function addContact(name, email, phone) {
+  fs.readFile(contactPatch)
+    .then((data) => {
+      let json = JSON.parse(data);
+      json.push({
+        id: json.length + 1,
+        name: name,
+        email: email,
+        phone: phone,
+      });
+
+      fs.writeFile(contactPatch, JSON.stringify(json))
+        .then(() => {
+          console.log("Append Success");
+        })
+        .catch((err) => {
+          console.log("Append Failed: " + err);
+        });
+    })
+    .catch((err) => {
+      console.log("Read Error: " + err);
+    });
+  return;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
 };
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const {listContacts, getContactById, removeContact, addContact} = require('./contacts')
 
-getContactById(1);
+//getContactById(3)
+//listContacts()
+removeContact(88)
+//addContact('Mango', 'mango@gmail.com', '322-22-22')
